a41aa679a84cb597e20ab9c48a318201bede098e
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:49:22 -0700

    Clean up unused functions from fft wrappers

--- src/trans/gpu/internal/sufft_mod.F90
+++ src/trans/gpu/internal/sufft_mod.F90
@@ -1,5 +1,5 @@
 ! (C) Copyright 2000- ECMWF.
-! (C) Copyright 2000- Meteo-France.
+! (C) Copyright 2022- NVIDIA.
 ! 
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -12,36 +12,45 @@ MODULE SUFFT_MOD
   CONTAINS
   SUBROUTINE SUFFT
   
-  USE PARKIND1      ,ONLY : JPIM
+  USE PARKIND1  ,ONLY : JPIM
   
-  USE TPM_DIM       ,ONLY : R
-  USE TPM_GEN       ,ONLY : NOUT, NPRINTLEV
-  USE TPM_DISTR     ,ONLY : D, MYSETW
-  USE TPM_GEOMETRY  ,ONLY : G
-  USE TPM_FFT       ,ONLY : T
-  USE TPM_FFTC      ,ONLY : TC, INIT_PLANS_FFT
+  USE TPM_DIM         ,ONLY : R
+  USE TPM_GEN         ,ONLY : NOUT, NPRINTLEV
+  USE TPM_DISTR       ,ONLY : D, MYSETW
+  USE TPM_GEOMETRY    ,ONLY : G
+  USE TPM_FFT         ,ONLY : T
+#ifdef WITH_FFTW
+  USE TPM_FFTW        ,ONLY : TW, INIT_PLANS_FFTW
+#endif
   !
- 
+  
   IMPLICIT NONE
- 
+  
   INTEGER(KIND=JPIM) :: JGL,IGLG
   LOGICAL :: LLP1,LLP2
- 
+  
   !     ------------------------------------------------------------------
- 
+  
   IF(.NOT.D%LGRIDONLY) THEN
- 
+  
     LLP1 = NPRINTLEV>0
     LLP2 = NPRINTLEV>1
     IF(LLP1) WRITE(NOUT,*) '=== ENTER ROUTINE SUFFT ==='
 
-    CALL INIT_PLANS_FFT(R%NDLON)
- 
+#ifdef WITH_FFTW
+    IF(TW%LFFTW)THEN
+      CALL INIT_PLANS_FFTW(R%NDLON)
+    ELSE
+      NULLIFY(TW%FFTW_PLANS)
+      !NULLIFY(TW%N_PLANS)
+    ENDIF
+#endif
+  
   ENDIF
- 
+  
   !     ------------------------------------------------------------------
- 
+  
   9 FORMAT(1X,'ARRAY ',A10,' ALLOCATED ',8I8)
- 
+  
   END SUBROUTINE SUFFT
-END MODULE SUFFT_MOD  
+  END MODULE SUFFT_MOD  
