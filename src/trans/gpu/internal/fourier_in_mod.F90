1d6a3889d46c95f1048f455e016a86a4688dba4b
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:48:53 -0700

    tight packing in trmtol/trltom for FOUBUF/FOUBUF_IN

--- src/trans/gpu/internal/fourier_in_mod.F90
+++ src/trans/gpu/internal/fourier_in_mod.F90
@@ -1,5 +1,5 @@
 ! (C) Copyright 2000- ECMWF.
-! (C) Copyright 2000- Meteo-France.
+! (C) Copyright 2022- NVIDIA.
 ! 
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -38,12 +38,12 @@ SUBROUTINE FOURIER_IN(PREEL,KFIELDS)
 
 !     ------------------------------------------------------------------
 
-USE PARKIND_ECTRANS ,ONLY : JPIM     ,JPRBT
+USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRBT
 
 USE TPM_DISTR       ,ONLY : D, MYSETW, MYPROC, NPROC, D_NSTAGTF,D_MSTABF,D_NSTAGT0B,D_NPNTGTB0,D_NPROCM,D_NPTRLS
 USE TPM_TRANS       ,ONLY : FOUBUF
 USE TPM_GEOMETRY    ,ONLY : G, G_NMEN,G_NMEN_MAX
-USE TPM_GEN         ,ONLY : NOUT
+use tpm_gen, only: nout
 !
 
 IMPLICIT NONE
@@ -74,27 +74,55 @@ ENDIF
 DO KGL=IBEG,IEND,IINC
    DO JM=0,G_NMEN_MAX      
       DO JF=1,KFIELDS     
- 
+         
          IGLG = D_NPTRLS(MYSETW)+KGL-1
-
-         IF ( JM .LE. G_NMEN(IGLG)) THEN
-
+         
+         if ( JM .le. G_NMEN(IGLG)) then
+            
             IPROC = D_NPROCM(JM)
-            ISTA  = (D_NSTAGT0B(D_MSTABF(IPROC))+D_NPNTGTB0(JM,KGL))*2*KFIELDS
-
+            ISTA  = (D_NSTAGT0B(IPROC)+D_NPNTGTB0(JM,KGL))*2*KFIELDS
+            
             PREEL(2*JF-1,2*JM+1+D_NSTAGTF(KGL)) = FOUBUF(ISTA+2*JF-1)
             PREEL(2*JF,  2*JM+1+D_NSTAGTF(KGL)) = FOUBUF(ISTA+2*JF  )
+            !write(nout,*) , 'istart1 ...', KGL, JM, JF, ISTA+2*JF,ISTA,D_NSTAGT0B(D_MSTABF(IPROC)),IPROC,KFIELDS
+            !write(nout,*) , 'istart2 ...',D_NPNTGTB0(JM,KGL), FOUBUF(ISTA+2*JF-1), FOUBUF(ISTA+2*JF),2*JM+1+D_NSTAGTF(KGL)
+            !if(jf==1 .and. 2*JM+1+D_NSTAGTF(KGL)==7972) write(nout,*) 'fourier_in: fidx=7972, kgl=',kgl,' jm=',jm
             !TODO (Andreas): should be able to remove the factor 2 in the second dimension (in front of jm)
             !and reduce the size of the array. Will need to adapt fsc_mod accordingly! This is actually more
             !difficult: d_nstagtf(kgl) is not necessarily even!
- 
-         END IF
+            
+         end if
       ENDDO
    ENDDO
 ENDDO
 !$ACC END DATA
 
+!iimax1=0
+!iimax2=0
+!iimax3=0
+!iunit=myproc+300
+!DO KGL=IBEG,IEND,IINC
+!   DO JM=0,G_NMEN_MAX      
+!      DO JF=1,KFIELDS     
+!         
+!         IGLG = D_NPTRLS(MYSETW)+KGL-1
+!         
+!         if ( JM .le. G_NMEN(IGLG)) then
+!            
+!            IPROC = D_NPROCM(JM)
+!            ISTA  = (D_NSTAGT0B(D_MSTABF(IPROC))+D_NPNTGTB0(JM,KGL))*2*KFIELDS
+!           
+!            iimax1=max(iimax1,2*JF)
+!            iimax2=max(iimax2,2*JM+1+D_NSTAGTF(KGL))
+!            iimax3=max(iimax3,ISTA+2*JF)
+!
+!          endif
+!      ENDDO 
+!    ENDDO 
+!ENDDO 
+!write(iunit,*) 'max_in ',iimax1,size(PREEL,1),iimax2,size(PREEL,2),iimax3,size(FOUBUF)
 !     ------------------------------------------------------------------
 
 END SUBROUTINE FOURIER_IN
 END MODULE FOURIER_IN_MOD
+
