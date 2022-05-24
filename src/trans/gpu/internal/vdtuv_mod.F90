14eae0c3b0beba9254db61d7c6ffc3ddf83dcc56
Lukas Mosimann <lmosimann@nvidia.com>
2022-05-24 05:48:58 -0700

    Remove ZN

--- src/trans/gpu/internal/vdtuv_mod.F90
+++ src/trans/gpu/internal/vdtuv_mod.F90
@@ -1,5 +1,5 @@
 ! (C) Copyright 2000- ECMWF.
-! (C) Copyright 2000- Meteo-France.
+! (C) Copyright 2022- NVIDIA.
 ! 
 ! This software is licensed under the terms of the Apache Licence Version 2.0
 ! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -12,12 +12,12 @@ MODULE VDTUV_MOD
 CONTAINS
 SUBROUTINE VDTUV(KFIELD,PEPSNM,PVOR,PDIV,PU,PV)
 
-USE PARKIND_ECTRANS ,ONLY : JPIM     ,JPRB,  JPRBT
+USE PARKIND_ECTRANS  ,ONLY : JPIM     ,JPRB,  JPRBT
 
-USE TPM_DIM         ,ONLY : R
+USE TPM_DIM         ,ONLY : R, R_NTMAX
 USE TPM_FIELDS      ,ONLY : F
 USE TPM_DISTR       ,ONLY : D,D_NUMP,D_MYMS
-USE TPM_GEN         ,ONLY : NOUT
+use tpm_gen, only: nout
 
 
 !**** *VDTUV* - Compute U,V in  spectral space
@@ -78,7 +78,7 @@ IMPLICIT NONE
 INTEGER(KIND=JPIM) :: KM, kmloc
 INTEGER(KIND=JPIM), INTENT(IN) :: KFIELD
 REAL(KIND=JPRBT), INTENT(IN)    :: PEPSNM(1:D%NUMP,0:R%NTMAX+2)
-REAL(KIND=JPRB), INTENT(IN)    :: PVOR(:,:,:),PDIV(:,:,:)
+REAL(KIND=JPRB), INTENT(INOUT)    :: PVOR(:,:,:),PDIV(:,:,:)
 REAL(KIND=JPRB), INTENT(OUT)   :: PU  (:,:,:),PV  (:,:,:)
 
 !     LOCAL INTEGER SCALARS
@@ -86,15 +86,14 @@ INTEGER(KIND=JPIM) :: II, IJ, IR, J, JN, ISMAX,JI
 
 !     LOCAL REAL SCALARS
 REAL(KIND=JPRBT) :: ZKM
-REAL(KIND=JPRBT) :: ZN(-1:R%NTMAX+4)
 REAL(KIND=JPRBT) :: ZLAPIN(-1:R%NSMAX+4)
 REAL(KIND=JPRBT) :: ZEPSNM(-1:R%NSMAX+4)
 
 !$ACC DATA                                     &
-!$ACC      CREATE (ZEPSNM, ZN, ZLAPIN)         &
+!$ACC      CREATE (ZEPSNM, ZLAPIN)         &
 !$ACC      COPYIN (D,D%MYMS,F,F%RLAPIN,F%RN)   &
 !$ACC      PRESENT(PEPSNM, PVOR, PDIV)         &
-!$ACC      PRESENT(PU, PV)
+!$ACC      PRESENT(PU, PV, D_MYMS)
 
 
 !     ------------------------------------------------------------------
@@ -104,35 +103,34 @@ REAL(KIND=JPRBT) :: ZEPSNM(-1:R%NSMAX+4)
 
 ISMAX = R%NSMAX
 DO KMLOC=1,D%NUMP
-  ZKM = D%MYMS(KMLOC)
+  KM = D_MYMS(KMLOC)
+  ZKM = REAL(KM,JPRBT)
   !$ACC PARALLEL LOOP DEFAULT(NONE)
-  DO JN=ZKM-1,ISMAX+2
+  DO JN=KM-1,ISMAX+2
     IJ = ISMAX+3-JN
-    ZN(IJ) = F%RN(JN)
     ZLAPIN(IJ) = F%RLAPIN(JN)
     IF( JN >= 0 ) THEN
-        ZEPSNM(IJ) = PEPSNM(KMLOC,JN) 
+        ZEPSNM(ISMAX+3-JN) = PEPSNM(KMLOC,JN) 
     ELSE
-        ZEPSNM(IJ) = 0
+        ZEPSNM(ISMAX+3-JN) = 0
     ENDIF
   ENDDO
-  !$ACC KERNELS DEFAULT(NONE)
-  ZN(0) = F%RN(ISMAX+3)
-  !$ACC END KERNELS
 
 !*       1.1      U AND V (KM=0) .
 
-IF(ZKM == 0) THEN
+IF(KM == 0) THEN
   !$ACC PARALLEL LOOP DEFAULT(NONE)
   DO J=1,KFIELD
     IR = 2*J-1
-    DO JI=2,ISMAX+3
+
+    DO JN=0,R_NTMAX+1
+      JI = R_NTMAX+3-JN
       PU(IR,JI,KMLOC) = +&
-      &ZN(JI+1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PVOR(IR,JI+1,KMLOC)-&
-      &ZN(JI-2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PVOR(IR,JI-1,KMLOC)
+      &(JN-1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PVOR(IR,JI+1,KMLOC)-&
+      &(JN+2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PVOR(IR,JI-1,KMLOC)
       PV(IR,JI,KMLOC) = -&
-      &ZN(JI+1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PDIV(IR,JI+1,KMLOC)+&
-      &ZN(JI-2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PDIV(IR,JI-1,KMLOC)
+      &(JN-1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PDIV(IR,JI+1,KMLOC)+&
+      &(JN+2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PDIV(IR,JI-1,KMLOC)
     ENDDO
   ENDDO
 ELSE
@@ -140,23 +138,24 @@ ELSE
 
     !$ACC PARALLEL LOOP COLLAPSE(2) DEFAULT(NONE)
     DO J=1,KFIELD
-      DO JI=2,ISMAX+3-ZKM
-        !ZKM = D_MYMS(KMLOC)
+
+      DO JN=KM,R_NTMAX+1
+        JI = R_NTMAX+3-JN
         IR = 2*J-1
         II = IR+1
         !IF (ZKM>0 .AND. JI<=ISMAX+3-zKM) THEN
-          PU(IR,JI,KMLOC) = -ZKM*ZLAPIN(JI)*PDIV(II,JI,KMLOC)+&
-          &ZN(JI+1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PVOR(IR,JI+1,KMLOC)-&
-          &ZN(JI-2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PVOR(IR,JI-1,KMLOC)
-          PU(II,JI,KMLOC) = +ZKM*ZLAPIN(JI)*PDIV(IR,JI,KMLOC)+&
-          &ZN(JI+1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PVOR(II,JI+1,KMLOC)-&
-          &ZN(JI-2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PVOR(II,JI-1,KMLOC)
-          PV(IR,JI,KMLOC) = -ZKM*ZLAPIN(JI)*PVOR(II,JI,KMLOC)-&
-          &ZN(JI+1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PDIV(IR,JI+1,KMLOC)+&
-          &ZN(JI-2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PDIV(IR,JI-1,KMLOC)
-          PV(II,JI,KMLOC) = +ZKM*ZLAPIN(JI)*PVOR(IR,JI,KMLOC)-&
-          &ZN(JI+1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PDIV(II,JI+1,KMLOC)+&
-          &ZN(JI-2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PDIV(II,JI-1,KMLOC)
+          PU(ir,JI,kmloc) = -ZKM*ZLAPIN(JI)*PDIV(ii,JI,kmloc)+&
+          &(JN-1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PVOR(ir,JI+1,kmloc)-&
+          &(JN+2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PVOR(ir,JI-1,kmloc)
+          PU(ii,JI,kmloc) = +ZKM*ZLAPIN(JI)*PDIV(ir,JI,kmloc)+&
+          &(JN-1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PVOR(ii,JI+1,kmloc)-&
+          &(JN+2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PVOR(ii,JI-1,kmloc)
+          PV(ir,JI,kmloc) = -ZKM*ZLAPIN(JI)*PVOR(ii,JI,kmloc)-&
+          &(JN-1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PDIV(ir,JI+1,kmloc)+&
+          &(JN+2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PDIV(ir,JI-1,kmloc)
+          PV(ii,JI,kmloc) = +ZKM*ZLAPIN(JI)*PVOR(ir,JI,kmloc)-&
+          &(JN-1)*ZEPSNM(JI)*ZLAPIN(JI+1)*PDIV(ii,JI+1,kmloc)+&
+          &(JN+2)*ZEPSNM(JI-1)*ZLAPIN(JI-1)*PDIV(ii,JI-1,kmloc)
         !ENDIF
       ENDDO
     ENDDO
