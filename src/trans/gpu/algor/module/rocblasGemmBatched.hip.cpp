76c71f869243104d01f44c405381d4cb9841ecb6
Sam Hatfield <samuel.hatfield@ecmwf.int>
2023-03-16 11:52:24 +0100

    Consolidate GEMM HIP code
    I put the double- and single-precision versions in the same HIP file and simplified them a bit

--- src/trans/gpu/algor/module/rocblasGemmBatched.hip.cpp
+++ src/trans/gpu/algor/module/rocblasGemmBatched.hip.cpp
@@ -1,6 +1,7 @@
 //
-// Wrapper for hipblasSgemm function. 
+// Wrappers for rocblas_dgemm_batched and rocblas_sgemm_batched. 
 //
+
 #include <iostream>
 #include <stdio.h>
 #include "hip/hip_runtime_api.h"
@@ -18,7 +19,16 @@ using namespace std;
     }                                                                   \
   }
 
-bool alreadyAllocated_sgemm=false;
+bool alreadyAllocated_dgemm = false;
+bool alreadyAllocated_sgemm = false;
+
+double **d_Aarray_dgemm;
+double **d_Barray_dgemm;
+double **d_Carray_dgemm;
+
+double **Aarray_dgemm;
+double **Barray_dgemm;
+double **Carray_dgemm;
 
 float **d_Aarray_sgemm;
 float **d_Barray_sgemm;
@@ -34,6 +44,53 @@ rocblas_handle get_rocblas_handle() {
   return handle;
 }
 
+extern "C" void cublasDgemmBatched_wrapper (char transa, char transb,
+                                            int m, int n, int k,
+                                            double alpha,
+                                            const double *A, int lda, int tda,
+                                            const double *B, int ldb, int tdb,
+                                            double beta, 
+                                            double *C, int ldc, int tdc,
+                                            int batchCount)
+{
+  // Determine whether either input array should be transposed
+  rocblas_operation op_t1 = rocblas_operation_none, op_t2 = rocblas_operation_none;
+  if (transa == 'T' || transa == 't') op_t1 = rocblas_operation_transpose;
+  if (transb == 'T' || transb == 't') op_t2 = rocblas_operation_transpose;
+
+  rocblas_handle handle = get_rocblas_handle();
+
+  if (!alreadyAllocated_dgemm){
+    hipHostMalloc(&Aarray_dgemm, batchCount*sizeof(double*));
+    hipHostMalloc(&Barray_dgemm, batchCount*sizeof(double*));
+    hipHostMalloc(&Carray_dgemm, batchCount*sizeof(double*));
+
+    hipMalloc(&d_Aarray_dgemm, batchCount*sizeof(double*));
+    hipMalloc(&d_Barray_dgemm, batchCount*sizeof(double*));
+    hipMalloc(&d_Carray_dgemm, batchCount*sizeof(double*));
+   }
+  alreadyAllocated_dgemm = true;
+
+  for (int i=0; i < batchCount; i++) {
+    Aarray_dgemm[i] = (double*) &(A[i*lda*tda]);
+    Barray_dgemm[i] = (double*) &(B[i*ldb*tdb]);
+    Carray_dgemm[i] = (double*) &(C[i*ldc*tdc]);
+  }
+
+  hipMemcpy(d_Aarray_dgemm, Aarray_dgemm, batchCount*sizeof(double*), hipMemcpyHostToDevice);
+  hipMemcpy(d_Barray_dgemm, Barray_dgemm, batchCount*sizeof(double*), hipMemcpyHostToDevice);
+  hipMemcpy(d_Carray_dgemm, Carray_dgemm, batchCount*sizeof(double*), hipMemcpyHostToDevice);
+
+  ROCBLAS_CHECK(rocblas_dgemm_batched(handle, op_t1, op_t2, m, n, k,
+                                      &alpha,
+                                      (const double**) d_Aarray_dgemm, lda,
+                                      (const double**) d_Barray_dgemm, ldb,
+                                      &beta,
+                                      (double**) d_Carray_dgemm, ldc,
+                                      batchCount));
+  hipDeviceSynchronize();
+}
+
 extern "C" void cublasSgemmBatched_wrapper(char transa, char transb,
                                            int m, int n, int k,
                                            float alpha,
@@ -45,32 +102,31 @@ extern "C" void cublasSgemmBatched_wrapper(char transa, char transb,
 {
   // Determine whether either input array should be transposed
   rocblas_operation op_t1 = rocblas_operation_none, op_t2 = rocblas_operation_none;
-  if (transa=='T' || transa=='t') 
-    op_t1 = rocblas_operation_transpose;
-  if (transb=='T' || transb=='t') 
-    op_t2 = rocblas_operation_transpose;
+  if (transa == 'T' || transa == 't') op_t1 = rocblas_operation_transpose;
+  if (transb == 'T' || transb == 't') op_t2 = rocblas_operation_transpose;
 
   rocblas_handle handle = get_rocblas_handle();
 
-  if (!alreadyAllocated_sgemm){
-    hipHostMalloc(&Aarray_sgemm,batchCount*sizeof(float*));
-    hipHostMalloc(&Barray_sgemm,batchCount*sizeof(float*));
-    hipHostMalloc(&Carray_sgemm,batchCount*sizeof(float*));
-  }
-  alreadyAllocated_sgemm=true;
+  if (!alreadyAllocated_sgemm) {
+    hipHostMalloc(&Aarray_sgemm, batchCount*sizeof(float*));
+    hipHostMalloc(&Barray_sgemm, batchCount*sizeof(float*));
+    hipHostMalloc(&Carray_sgemm, batchCount*sizeof(float*));
 
-  hipMalloc(&d_Aarray_sgemm,batchCount*sizeof(float*));
-  hipMalloc(&d_Barray_sgemm,batchCount*sizeof(float*));
-  hipMalloc(&d_Carray_sgemm,batchCount*sizeof(float*));
+    hipMalloc(&d_Aarray_sgemm, batchCount*sizeof(float*));
+    hipMalloc(&d_Barray_sgemm, batchCount*sizeof(float*));
+    hipMalloc(&d_Carray_sgemm, batchCount*sizeof(float*));
+  }
+  alreadyAllocated_sgemm = true;
 
   for (int i = 0; i < batchCount; i++) {
     Aarray_sgemm[i] = (float*) &(A[i*lda*tda]);
     Barray_sgemm[i] = (float*) &(B[i*ldb*tdb]);
     Carray_sgemm[i] = (float*) &(C[i*ldc*tdc]);
   }
-  hipMemcpy(d_Aarray_sgemm,Aarray_sgemm,batchCount*sizeof(float*),hipMemcpyHostToDevice);
-  hipMemcpy(d_Barray_sgemm,Barray_sgemm,batchCount*sizeof(float*),hipMemcpyHostToDevice);
-  hipMemcpy(d_Carray_sgemm,Carray_sgemm,batchCount*sizeof(float*),hipMemcpyHostToDevice);
+
+  hipMemcpy(d_Aarray_sgemm, Aarray_sgemm, batchCount*sizeof(float*), hipMemcpyHostToDevice);
+  hipMemcpy(d_Barray_sgemm, Barray_sgemm, batchCount*sizeof(float*), hipMemcpyHostToDevice);
+  hipMemcpy(d_Carray_sgemm, Carray_sgemm, batchCount*sizeof(float*), hipMemcpyHostToDevice);
 
   ROCBLAS_CHECK(rocblas_sgemm_batched(handle, op_t1, op_t2, m, n, k,
                                       &alpha,
@@ -81,12 +137,9 @@ extern "C" void cublasSgemmBatched_wrapper(char transa, char transb,
                                       batchCount));
 
   hipDeviceSynchronize();
-  
-  hipFree(d_Aarray_sgemm);
-  hipFree(d_Barray_sgemm);
-  hipFree(d_Carray_sgemm);
 }
 
+// This should be implemented eventually, and also for DGEMM
 // extern "C" void cublasSgemmStridedBatched_wrapper(char transa, char transb,
 //                                                   int m, int n, int k,
 //                                                   float alpha,
