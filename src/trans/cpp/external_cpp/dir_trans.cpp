48859afa4e2fe6282b6ccf34ea347903f9d602c6
Sam Hatfield <samuel.hatfield@ecmwf.int>
2025-12-16 14:17:02 +0000

    Add initial test implementation of C++ backend

---
+++ src/trans/cpp/external_cpp/dir_trans.cpp
@@ -0,0 +1,39 @@
+// (C) Copyright 2025- ECMWF.
+//
+// This software is licensed under the terms of the Apache Licence Version 2.0
+// which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+// In applying this licence, ECMWF does not waive the privileges and immunities
+// granted to it by virtue of its status as an intergovernmental organisation
+// nor does it submit to any jurisdiction.
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+extern "C" {
+void dir_trans_sp(
+    float* pspvor, float* pspdiv,
+    float* pspscalar, float* pspsc3a, float* pspsc3b, float* pspsc2,
+    bool ldlatlon, int kproma,
+    const int* kvsetuv, const int* kvsetsc,
+    int kresol,
+    const int* kvsetsc3a, const int* kvsetsc3b, const int* kvsetsc2,
+    const float* pgp,
+    const float* pgpuv, const float* pgp3a, const float* pgp3b, const float * pgp2) {
+
+    std::cout << "Inside dir_trans_sp" << std::endl;
+}
+
+void dir_trans_dp(
+    double* pspvor, double* pspdiv,
+    double* pspscalar, double* pspsc3a, double* pspsc3b, double* pspsc2,
+    bool ldlatlon, int kproma,
+    const int* kvsetuv, const int* kvsetsc,
+    int kresol,
+    const int* kvsetsc3a, const int* kvsetsc3b, const int* kvsetsc2,
+    const double* pgp,
+    const double* pgpuv, const double* pgp3a, const double* pgp3b, const double * pgp2) {
+
+    std::cout << "Inside dir_trans_dp" << std::endl;
+}
+}
