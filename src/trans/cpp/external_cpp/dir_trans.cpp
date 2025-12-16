c9102abec261aec813f54800c87402326985c167
Sam Hatfield <samuel.hatfield@ecmwf.int>
2025-12-16 17:54:46 +0000

    (work in progress)

--- src/trans/cpp/external_cpp/dir_trans.cpp
+++ src/trans/cpp/external_cpp/dir_trans.cpp
@@ -10,7 +10,19 @@
 #include <stdio.h>
 #include <iostream>
 
+//  struct args_info{
+//    int* ispvor_shape, ispdiv_shape, ispscalar_shape, ispsc3a_shape, ispsc3b_shape, ispsc2_shape;
+//    int* ivsetuv_shape, ivsetsc_shape, ivsetsc3a_shape, ivsetsc3b_shape, ivsetsc2_shape;
+//    int* igp_shape, igpuv_shape, igp3a_shape, igp3b_shape, igp2_shape;
+//  };
+
+struct args_info{
+  int ispvor_shape[2];
+  int ivsetuv_shape[1];
+};
+
 template <typename Real> void dir_trans(
+  args_info args,
   Real* pspvor, Real* pspdiv,
   Real* pspscalar, Real* pspsc3a, Real* pspsc3b, Real* pspsc2,
   bool ldlatlon, int kproma,
@@ -25,6 +37,14 @@ template <typename Real> void dir_trans(
   } else {
     std::cout << "Inside dir_trans_dp" << std::endl;
   }
+
+  if (kvsetuv) {
+    std::cout << " kvsetuv is present" << std::endl;
+    // std::cout << args.ispvor_shape[0] << std::endl;
+    // // std::cout << args.ispvor_shape[1] << std::endl;
+    // std::cout << args.ivsetuv_shape[0] << std::endl;
+    std::cout << " done" << std::endl;
+  }
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -33,6 +53,7 @@ template <typename Real> void dir_trans(
 
 extern "C" {
   void dir_trans_sp(
+    args_info args,
     float* pspvor, float* pspdiv,
     float* pspscalar, float* pspsc3a, float* pspsc3b, float* pspsc2,
     bool ldlatlon, int kproma,
@@ -43,6 +64,7 @@ extern "C" {
     const float* pgpuv, const float* pgp3a, const float* pgp3b, const float * pgp2) {
 
     dir_trans(
+      args,
       pspvor, pspdiv,
       pspscalar, pspsc3a, pspsc3b, pspsc2,
       ldlatlon, kproma,
@@ -55,6 +77,7 @@ extern "C" {
   }
 
   void dir_trans_dp(
+    args_info args,
     double* pspvor, double* pspdiv,
     double* pspscalar, double* pspsc3a, double* pspsc3b, double* pspsc2,
     bool ldlatlon, int kproma,
@@ -65,6 +88,7 @@ extern "C" {
     const double* pgpuv, const double* pgp3a, const double* pgp3b, const double * pgp2) {
 
     dir_trans(
+      args,
       pspvor, pspdiv,
       pspscalar, pspsc3a, pspsc3b, pspsc2,
       ldlatlon, kproma,
