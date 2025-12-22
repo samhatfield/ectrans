7deccda6f0cfdc6d5c10daa1a0bcd6be9b9676a2
Sam Hatfield <samuel.hatfield@ecmwf.int>
2025-12-22 17:31:11 +0000

    Remove unnecessary includes

--- src/trans/cpp/external_cpp/setup_trans0.cpp
+++ src/trans/cpp/external_cpp/setup_trans0.cpp
@@ -6,8 +6,6 @@
 // granted to it by virtue of its status as an intergovernmental organisation
 // nor does it submit to any jurisdiction.
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 
 #include "abor1.h"
